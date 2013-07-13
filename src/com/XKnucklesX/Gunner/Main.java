e0a65493b7ba99669bbdb382cb46ed1930e7bb9d
XKnucklesX <needsdeveloping@gmail.com>
2013-07-13 17:50:03 +0800

    Update Main.java

--- src/com/XKnucklesX/Gunner/Main.java
+++ src/com/XKnucklesX/Gunner/Main.java
@@ -1,37 +1,57 @@
-package me.XKnucklesX.Needs ;
- 
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
+package com.xknucklesx.Guns;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Egg;
+import org.bukkit.entity.EnderPearl;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
  
- 
-public class Needs extends JavaPlugin {
-   
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-       
-        if (!(sender instanceof Player )) {
-            sender.sendMessage("The console ran the needs command!"); //Broadcasts the message when console uses command
-            return true;
-        }
-       
-        Player player = (Player) sender;
-       
-        if (cmd.getName().equalsIgnoreCase("tips")) {
-            player.sendMessage(ChatColor.AQUA + "This Servers tips are: 1./n 2./n 3./n 4./n 5./n"); //Shows the list of tips for current server
-            return true;
-        } else if (cmd.getName().equalsIgnoreCase("needs")) {
-            player.sendMessage(ChatColor.GREEN + "Manager: Nelsin/n Project Lead: Igorvanloo/n Developers:"); //Shows list of Developers
-            return true;
-        } else if (cmd.getName().equalsIgnoreCase("hello")) {
-        	player.sendMessage(ChatColor.GREEN + "Hello");
-            return true;
-        } else if (cmd.getName().equalsIgnoreCase("Hi")) {
-         player.sendMessage(ChatColor.AQUA + "Hi");
-        	return true;
-        } else {
-         return false;
-    }  
+	
+	@Override 
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@Override
+	public void onDisable() {
+		saveConfig();
+	}
+	
+	@EventHandler
+	public void onClick(PlayerInteractEvent e) {
+		if(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK);
+		Player p = e.getPlayer();
+		if(p.getItemInHand().getType() == Material.WOOD_HOE) {
+			Gun(p);
+			
+		}
+	}
+	
+	@EventHandler
+	public void onClick2(PlayerInteractEvent e) {
+		if(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK);
+		Player p = e.getPlayer();
+		if(p.getItemInHand().getType() == Material.STONE_HOE) {
+			Gun1(p);
+		}
+	}
+
+	private void Gun1(Player p) {
+		EnderPearl ep = p.launchProjectile(EnderPearl.class);
+		ep.setVelocity(p.getLocation().getDirection().multiply(2.5));
+		
+	}
+
+	private void Gun(Player p) {
+		Arrow ar = p.launchProjectile(Arrow.class);
+		ar.setVelocity(p.getLocation().getDirection().multiply(2.0)); //Depends how far 
+	}
+		
 }
- 
